acc2ed71157ff37c43eb6d1b7d0b897d31b41322
Sosnowski0304 <rafael.sosnowski@gmail.com>
2020-03-24 01:06:46 -0300

    Revert "Removendo arquivos da raiz que foram movidos para a pasta backend"
    This reverts commit a6855f6948d0c31a927d4be3808e03cf6d8304da.

---
+++ src/app/controllers/DeliverymanController.js
@@ -0,0 +1,113 @@
+import * as Yup from 'yup';
+import Deliveryman from '../models/Deliveryman';
+import File from '../models/File';
+
+class DeliverymanController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const deliverymanExists = await Deliveryman.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (deliverymanExists) {
+      return res.status(400).json({ error: 'Delivery man alredy exists' });
+    }
+
+    const { id, name, email } = await Deliveryman.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+    });
+  }
+
+  async index(req, res) {
+    const { page = 1 } = req.query;
+
+    const couriers = await Deliveryman.findAll({
+      attributes: ['id', 'name', 'email', 'avatar_id'],
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['name', 'path', 'url'],
+        },
+      ],
+      order: ['name'],
+      limit: 20,
+      offset: (page - 1) * 20,
+    });
+
+    return res.json(couriers);
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const deliveryman = await Deliveryman.findByPk(req.params.id);
+
+    if (!deliveryman) {
+      return res.status(400).json({ error: 'Delivery man does not exists' });
+    }
+
+    const { email } = req.body;
+
+    if (email && email !== deliveryman.email) {
+      const deliverymanExists = await Deliveryman.findOne({ where: { email } });
+
+      if (deliverymanExists) {
+        return res.status(400).json({ error: 'Delivery man alredy exists.' });
+      }
+    }
+
+    const deliverymanUp = await deliveryman.update(req.body);
+
+    return res.json({
+      id: deliverymanUp.id,
+      name: deliverymanUp.name,
+      email: deliverymanUp.email,
+    });
+  }
+
+  async delete(req, res) {
+    const schema = Yup.object().shape({
+      id: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.params))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const deliveryman = await Deliveryman.findByPk(req.params.id);
+
+    if (!deliveryman) {
+      return res.status(400).json({ error: 'Delivery man does not exists' });
+    }
+
+    // await deliveryman.destroy();
+
+    return res
+      .status(200)
+      .json({ sucess: 'Delivery man successfully deleted' });
+  }
+}
+
+export default new DeliverymanController();
