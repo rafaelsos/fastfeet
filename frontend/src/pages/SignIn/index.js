d033fed9ccd7c1bf3b82b9561f00047880e0fae0
Sosnowski0304 <rafael.sosnowski@gmail.com>
2020-03-23 21:26:38 -0300

    frontend: criado routes/index.js, sevices/history.js, criado a pages/SignIn

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function SignIn() {
-  return <div />;
+  return (
+    <Container>
+      <h1>SignIn</h1>
+    </Container>
+  );
 }
