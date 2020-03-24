0817bd4fff9f8d65cb62e037ec51459e3ae655cf
Sosnowski0304 <rafael.sosnowski@gmail.com>
2020-03-23 23:47:29 -0300

    frontend: criado global style, criado pages/SignIn

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Content } from './styles';
+
+import logo from '~/assets/fastfeet-logo.png';
 
 export default function SignIn() {
   return (
     <Container>
-      <h1>Nicole</h1>
+      <Content>
+        <img src={logo} alt="FastFeet" />
+
+        <form>
+          <p>SEU E-MAIL</p>
+          <input type="email" placeholder="exemplo@fastfeet.com" />
+
+          <p>SUA SENHA</p>
+          <input type="password" placeholder="******" />
+
+          <button type="submit">Acessar sistema</button>
+        </form>
+      </Content>
     </Container>
   );
 }
--- frontend/src/pages/SignIn/styles.js
+++ frontend/src/pages/SignIn/styles.js
@@ -1,7 +1,65 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Container = styled.div`
-  flex: 1;
+  height: 100%;
   background: #7159c1;
-  color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: 315px;
+  text-align: center;
+  background: #fff;
+  padding: 20px;
+  border-radius: 4px;
+
+  img {
+    margin-top: 15px;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 15px;
+
+    p {
+      display: flex;
+      margin-top: 30px;
+      flex-direction: row;
+      font-size: 16px;
+      font-weight: bold;
+    }
+
+    input {
+      margin-top: 10px;
+      background: #fff;
+      border: 1px solid #999;
+      border-radius: 4px;
+      height: 44px;
+      padding: 0 15px;
+      color: #111;
+      &::placeholder {
+        color: #999;
+      }
+    }
+
+    button {
+      margin: 15px 0 0;
+      height: 44px;
+      background: #7159c1;
+      font-weight: bold;
+      color: #fff;
+      border: 0;
+      border-radius: 4px;
+      font-size: 16px;
+      transition: background 0.2s;
+      &:hover {
+        background: ${darken(0.03, '#ab59c1')};
+      }
+    }
+  }
 `;
